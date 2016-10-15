33e82cd05594c900d99535edbfdee77140523a40
Pawel Rawski <pawel@rawskys.com>
2016-10-15 20:49:34 +0200

    Fix previous

--- oauth2/app/controllers/OAuth2.scala
+++ oauth2/app/controllers/OAuth2.scala
@@ -22,6 +22,7 @@ class OAuth2 @Inject()(dataHandler: OAuthDataHandler) extends Controller with OA
 		authorize(dataHandler) { authInfo =>
 			Future.successful(Ok(Json.obj("id" -> authInfo.user.id)).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*"))
 		}.map(_.withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*"))
+	}
 
 	def accountOptions = Action { request =>
 		Ok(Json.obj()).withHeaders(
